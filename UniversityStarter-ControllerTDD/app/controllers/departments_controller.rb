67ca079ace5e7cd53bb0146448ee1fe099b2bfc4
aissaoui22 <aissaoui@andrew.cmu.edu>
2024-01-30 19:28:04 +0300

    solution added

---
+++ UniversityStarter-ControllerTDD/app/controllers/departments_controller.rb
@@ -0,0 +1,196 @@
+class DepartmentsController < ApplicationController
+    before_action :set_department, only: [:show, :edit]
+  
+    # GET /departments
+    def index
+      @active_departments = Department.all
+    end
+  
+    # GET /departments/1
+    def show
+    end
+  
+    # GET /departments/new
+    def new
+      @department = Department.new
+    end
+  
+    # GET /departments/1/edit
+    def edit
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_department
+      @department = Department.find(params[:id])
+    end
+  end
+  class DepartmentsController < ApplicationController
+    before_action :set_department, only: [:show, :edit]
+  
+    # GET /departments
+    def index
+      @active_departments = Department.all
+    end
+  
+    # GET /departments/1
+    def show
+    end
+  
+    # GET /departments/new
+    def new
+      @department = Department.new
+    end
+  
+    # GET /departments/1/edit
+    def edit
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_department
+      @department = Department.find(params[:id])
+    end
+  end
+  class DepartmentsController < ApplicationController
+    before_action :set_department, only: [:show, :edit]
+  
+    # GET /departments
+    def index
+      @active_departments = Department.all
+    end
+  
+    # GET /departments/1
+    def show
+    end
+  
+    # GET /departments/new
+    def new
+      @department = Department.new
+    end
+  
+    # GET /departments/1/edit
+    def edit
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_department
+      @department = Department.find(params[:id])
+    end
+  end
+  class DepartmentsController < ApplicationController
+    before_action :set_department, only: [:show, :edit]
+  
+    # GET /departments
+    def index
+      @active_departments = Department.all
+    end
+  
+    # GET /departments/1
+    def show
+    end
+  
+    # GET /departments/new
+    def new
+      @department = Department.new
+    end
+  
+    # GET /departments/1/edit
+    def edit
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_department
+      @department = Department.find(params[:id])
+    end
+  end
+  class DepartmentsController < ApplicationController
+    before_action :set_department, only: [:show, :edit]
+  
+    # GET /departments
+    def index
+      @active_departments = Department.all
+    end
+  
+    # GET /departments/1
+    def show
+    end
+  
+    # GET /departments/new
+    def new
+      @department = Department.new
+    end
+  
+    # GET /departments/1/edit
+    def edit
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_department
+      @department = Department.find(params[:id])
+    end
+  end
+  class DepartmentsController < ApplicationController
+    before_action :set_department, only: [:show, :edit]
+  
+    # GET /departments
+    def index
+      @active_departments = Department.all
+    end
+  
+    # GET /departments/1
+    def show
+    end
+  
+    # GET /departments/new
+    def new
+      @department = Department.new
+    end
+  
+    # GET /departments/1/edit
+    def edit
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_department
+      @department = Department.find(params[:id])
+    end
+  end
+  class DepartmentsController < ApplicationController
+    before_action :set_department, only: [:show, :edit]
+  
+    # GET /departments
+    def index
+      @active_departments = Department.all
+    end
+  
+    # GET /departments/1
+    def show
+    end
+  
+    # GET /departments/new
+    def new
+      @department = Department.new
+    end
+  
+    # GET /departments/1/edit
+    def edit
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_department
+      @department = Department.find(params[:id])
+    end
+  end              
